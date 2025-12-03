9e7a7321183acd6107276a7ad7d8d7d9827f8e7b
tung-ideapad <tung-ideapad@staas.io>
2025-12-03 17:45:01 +0700

    * Version:     1.5.0

--- maika-genius/maika-genius.php
+++ maika-genius/maika-genius.php
@@ -3,7 +3,7 @@
  * Plugin Name: Maika Genius
  * Plugin URI:  https://www.askmaika.ai/maika-genius/
  * Description: Tired of spending hours writing product descriptions and optimizing your website? Maika Genius is the Al-powered solution that empowers you to create engaging content, boost SEO, and drive sales, all with the power of cutting-edge Generative Al.
- * Version:     1.4.1
+ * Version:     1.5.0
  * Author:      tomaskmaika
  * Author URI:  https://www.askmaika.ai
  * Text Domain: maika-genius
